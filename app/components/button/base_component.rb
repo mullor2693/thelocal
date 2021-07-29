65bead9c9baf190513a1d5018caa08d42ce5980c
Adri Mullor <mullor2693ggmail.com>
2021-07-29 04:33:56 +0200

    a litlte stlyling before close

--- app/components/button/base_component.rb
+++ app/components/button/base_component.rb
@@ -1,22 +1,47 @@
 class Button::BaseComponent < ViewComponent::Base
-	def initialize(title:nil, disabled:nil, color:nil, status:nil, data:nil, style:nil, size:nil, classes:nil)
-		# BTN_STYLES = [:default, :round, :fab, :icon, :simple]
+	def initialize(title:nil, icon:nil, disabled:nil, color:nil, status:nil, data:nil, style:nil, size:nil, classes:nil, responsive:true, name:nil, type:nil)
+		
+		btn_styles = ['default', 'round', 'fab', 'icon', 'icon-r', 'simple']
+		responsive_styles = ['icon-r', 'icon', 'round', 'default']
 		# BTN_SIZE = [:default,:small, :large]
-		@style = style || "default"
-		@title = title || "title"
+		
+		@responsive = !!responsive
+		@style = (!style.nil? && btn_styles.include?(style)) ? style : 'default'
+		is_responsive = !!@responsive && responsive_styles.include?(@style) && icon.present?
+		
+		@base_title = title || "title"
+
+		span_header = is_responsive ? '<span class="d-none d-sm-inline-block">' : '<span>'
+		@title = (span_header+@base_title+'</span>').html_safe
+		
+		@icon = icon || "undo"
+		icon_header = is_responsive ? '<i class="material-icons d-sm-none">' : '<i class="material-icons">'
+		
+		@fab_title = (icon_header+@icon+'</i>').html_safe
+		@icon_title = (icon_header+@icon+'</i>'+@title).html_safe
+		@iconr_title = (@title+icon_header+@icon+'</i>').html_safe
+		
+		@title = @fab_title if @style == 'fab'
+		@title = @icon_title if @style == 'icon' || is_responsive
+		@title = @iconr_title if @style == 'icon-r'
+		
 		@data = data || {}
 		@color = color
     @status = status || (@color.in?(ApplicationHelper::GLOBAL_STATUSES) ? @color : "none")
 		@class = "btn btn-#{@status}"
-		@class += " btn-fab btn-fab-mini btn-round" if @style == "fab" 
-		@class += " btn-round" if (@style == "icon" || @style == "round")
+		@class += " btn-fab btn-fab-mini btn-round" if @style == 'fab' 
+		@class += " btn-round" if (@style == 'icon' || @style == 'icon-r' || @style == 'round')
 		@class += " btn-sm" if @size == "small"
 		@class += " btn-lg" if @size == "large"
 		@class += " #{classes}" if classes
 		@disabled = !!disabled
+
+		@type = type || "button"
+		@name = name || "button"
+		
 	end
 
 	def call
-		button_tag(@title, type: 'button', class: @class, disabled: @disabled, data: @data)
+		button_tag(@title, class: @class, disabled: @disabled, data: @data, type: @type, name: @name)
 	end
 end
--- app/components/stimulus_components/reveal_component.rb
+++ app/components/stimulus_components/reveal_component.rb
@@ -6,7 +6,7 @@ class StimulusComponents::RevealComponent < Base::StaticComponent
   end
 
   def call
-    tag.div(class: "col-12", data: {controller: "reveal", reveal_hidden_class: 'd-none'}) do
+    tag.div(class: "w-100", data: {controller: "reveal", reveal_hidden_class: 'd-none'}) do
       tag.div(render(Button::BaseComponent.new(title: @title, color:'primary', classes:"pull-right", data: { action:"click->reveal#toggle", reveal_target: "item" })), class: "col-12") +
       tag.div(content, class: "d-none col-12", data: { reveal_target: "item" } )
     end
