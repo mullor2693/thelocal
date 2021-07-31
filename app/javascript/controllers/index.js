0306814cc30c1cd0f1da7fc3fdda8be35589d02b
Adri Mullor <mullor2693@gmail.com>
2021-07-31 02:02:43 +0200

    gitignore and taiwind

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -5,6 +5,7 @@ import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
 import Reveal from "stimulus-reveal-controller"
 import Flatpickr from 'stimulus-flatpickr'
+import { Alert, Autosave, Dropdown, Modal, Tabs, Popover, Toggle, Slideover } from "tailwindcss-stimulus-components"
 
 // Import style for flatpickr
 require("flatpickr/dist/flatpickr.css")
@@ -16,4 +17,11 @@ application.load(definitionsFromContext(context))
 application.register("reveal", Reveal)
 application.register('flatpickr', Flatpickr)
 
-
+application.register('alert', Alert)
+application.register('autosave', Autosave)
+application.register('dropdown', Dropdown)
+application.register('modal', Modal)
+application.register('tabs', Tabs)
+application.register('popover', Popover)
+application.register('toggle', Toggle)
+application.register('slideover', Slideover)
